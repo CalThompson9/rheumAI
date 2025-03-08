5f69080eb2c539d8418b0c170c40483ce1dcbfd3
joelenehales <joelenehales@bell.net>
2025-03-07 20:25:00 -0500

    Fix weird indenting

--- qt-app/concisesummaryformatter.h
+++ qt-app/concisesummaryformatter.h
@@ -6,16 +6,16 @@
  * @date Mar. 7, 2025
  */
 
- #ifndef CONCISESUMMARYFORMATTER_H
- #define CONCISESUMMARYFORMATTER_H
- 
- #include "summaryformatter.h"
- 
- class ConciseSummaryFormatter : public SummaryFormatter
- {
-     public:
-         void generateLayout(Summary& summary, QVBoxLayout* summaryLayout) const override;
-         ~ConciseSummaryFormatter() = default;  // Qt automatically manages memory of QObjects, no need for manual deletion
- };
- 
- #endif
+#ifndef CONCISESUMMARYFORMATTER_H
+#define CONCISESUMMARYFORMATTER_H
+
+#include "summaryformatter.h"
+
+class ConciseSummaryFormatter : public SummaryFormatter
+{
+    public:
+        void generateLayout(Summary& summary, QVBoxLayout* summaryLayout) const override;
+        ~ConciseSummaryFormatter() = default;  // Qt automatically manages memory of QObjects, no need for manual deletion
+};
+
+#endif
